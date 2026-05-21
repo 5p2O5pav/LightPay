dc32d12cb5051308ed83bf2789f667ab8a1f7bbc
5p2O5pav <aa316191@outlook.com>
2026-05-21 15:23:10 +0800

    Update tron.go

--- tron.go
+++ tron.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "io"
     "net/http"
+    "strconv"
     "time"
 )
 
@@ -12,25 +13,28 @@ type TronChain struct {
     config TronConfig
 }
 
-type TronConfig struct {
-    ApiKey  string   `yaml:"api_key"`
-    Network string   `yaml:"network"`
-    Wallets []string `yaml:"wallets"`
+type TronResponse struct {
+    Data []struct {
+        TransactionID string `json:"transaction_id"`
+        TokenInfo     struct {
+            Symbol string `json:"symbol"`
+        } `json:"token_info"`
+        From  string `json:"from"`
+        To    string `json:"to"`
+        Value string `json:"value"` // 十进制字符串，已含精度
+        Type  string `json:"type"`
+        BlockTimestamp int64 `json:"block_timestamp"`
+    } `json:"data"`
+    Meta struct{} `json:"meta"`
 }
 
 func (t *TronChain) Name() string { return "tron" }
-
-func (t *TronChain) GetWalletAddresses() []string {
-    return t.config.Wallets
-}
-
+func (t *TronChain) GetWalletAddresses() []string { return t.config.Wallets }
 func (t *TronChain) SelectWallet(orderID string) (string, error) {
-    // 公共的简单轮选，也可以在 config 中预设选择逻辑
     return SelectWalletFromList(t.config.Wallets, orderID), nil
 }
 
 func (t *TronChain) FetchRecentTransactions(address string, since time.Time) ([]IncomingTx, error) {
-    // 调用 TronGrid API，用 since 过滤时间
     url := fmt.Sprintf("https://api.trongrid.io/v1/accounts/%s/transactions/trc20?limit=20&min_timestamp=%d",
         address, since.UnixMilli())
     req, _ := http.NewRequest("GET", url, nil)
@@ -42,18 +46,29 @@ func (t *TronChain) FetchRecentTransactions(address string, since time.Time) ([]
     }
     defer resp.Body.Close()
     body, _ := io.ReadAll(resp.Body)
-    var result TronResponse
-    json.Unmarshal(body, &result)
-    // 解析成 []IncomingTx
-    return parseTronTransactions(result, address), nil
+    var tr TronResponse
+    json.Unmarshal(body, &tr)
+
+    var txs []IncomingTx
+    for _, d := range tr.Data {
+        if d.To == address && d.TokenInfo.Symbol == "USDT" {
+            amountFloat, err := strconv.ParseFloat(d.Value, 64)
+            if err != nil {
+                continue
+            }
+            // 转为最小单位（USDT 6 位小数）
+            amountInt := int64(amountFloat * 1e6)
+            txs = append(txs, IncomingTx{
+                TxID:   d.TransactionID,
+                To:     d.To,
+                Amount: amountInt,
+                Time:   time.Unix(d.BlockTimestamp/1000, 0),
+            })
+        }
+    }
+    return txs, nil
 }
 
 func (t *TronChain) EnsureAddressListener(address string) {
     ensureListenerForChain(t, address)
-}
-
-// 辅助：解析 TRON 返回的交易
-func parseTronTransactions(resp TronResponse, targetAddr string) []IncomingTx {
-    // 实现省略，提取 to == targetAddr 且 token='USDT' 的交易
-    // 返回 []IncomingTx
 }
