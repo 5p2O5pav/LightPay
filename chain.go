35a4b277c76ea18c5a700539b2354401fbe8bee1
5p2O5pav <aa316191@outlook.com>
2026-05-21 23:33:52 +0800

    Update chain.go

--- chain.go
+++ chain.go
@@ -6,13 +6,13 @@ type ChainHandler interface {
     Name() string
     GetWalletAddresses() []string
     SelectWallet(orderID string) (string, error)
-    FetchRecentTransactions(address string, since time.Time) ([]IncomingTx, error)
-    EnsureAddressListener(address string)
+    FetchRecentTransactions(address, token string, since time.Time) ([]IncomingTx, error)   // 增加 token
+    EnsureAddressListener(address, token string)                                            // 增加 token
 }
 
 type IncomingTx struct {
     TxID   string
     To     string
-    Amount int64   // 最小单位
+    Amount int64   // 内部精度（3位）
     Time   time.Time
 }
